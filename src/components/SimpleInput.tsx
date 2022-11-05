9248b4de1d249634fe5a1ac292c78a070c6cac52
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-06 00:44:30 +0900

    add useState, useRef in SimpleInput

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -1,3 +1,11 @@
+/* 
+  Validation Timing
+  1. When submitting the form => useRef might be better
+  2. When onBlur or every keystroke => useState is better
+
+  nameInputRef.current.value = ''; => manipulating DOM directly is NOT ideal.
+*/
+
 import {
   SimpleInputActionsDiv,
   SimpleInputButton,
@@ -5,13 +13,36 @@ import {
   SimpleInputInput,
   SimpleInputLabel,
 } from '@/components/SimpleInputStyle';
+import React, { useRef, useState } from 'react';
 
 function SimpleInput() {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef<HTMLInputElement>(null);
+
+  const nameInputChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`State: ${enteredName}`);
+    console.log(`Ref: ${nameInputRef.current?.value}`)
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <SimpleInputControlDiv>
         <SimpleInputLabel htmlFor="name">Your name</SimpleInputLabel>
-        <SimpleInputInput type="text" id="name" />
+        <SimpleInputInput
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </SimpleInputControlDiv>
       <SimpleInputActionsDiv>
         <SimpleInputButton>Submit</SimpleInputButton>
