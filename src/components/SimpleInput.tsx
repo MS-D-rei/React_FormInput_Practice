ac2e6c5ca14594faac831decae85ef0b481a1ddf
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-06 23:03:17 +0900

    add email input in SimpleInput

--- src/components/SimpleInput.tsx
+++ src/components/SimpleInput.tsx
@@ -22,10 +22,16 @@ function SimpleInput() {
   // const [isValidEnteredName, setIsValidEnteredName] = useState(false);
   const [isTouchedEnteredName, setIsTouchedEnteredName] = useState(false);
   // const [isValidForm, setIsValidForm] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [isTouchedEnteredEmail, setIsTouchedEnteredEmail] = useState(false);
 
   /* isValidEnteredName is derived from enteredName. When enteredName state is changed,
   isValidEnteredName is also re-evaluated */
   const isValidEnteredName = enteredName.trim().length !== 0;
+
+  const emailRegEx =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  const isValidEnteredEmail = enteredEmail.trim().match(emailRegEx);
 
   // useEffect(() => {
   //   if (isValidEnteredName) {
@@ -37,7 +43,7 @@ function SimpleInput() {
 
   /* isValidForm is derived from isValidEnteredName, there is no advantage from useEffect */
   let isValidForm = false;
-  if (isValidEnteredName) {
+  if (isValidEnteredName && isValidEnteredEmail) {
     isValidForm = true;
   }
 
@@ -51,6 +57,16 @@ function SimpleInput() {
     setIsTouchedEnteredName(true);
   };
 
+  const emailInputChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = () => {
+    setIsTouchedEnteredEmail(true);
+  };
+
   const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(`State: ${enteredName}`);
@@ -58,17 +74,24 @@ function SimpleInput() {
     setIsTouchedEnteredName(true);
   };
 
-  const controlDivClassName =
+  const nameControlDivClassName =
     !isValidEnteredName && isTouchedEnteredName ? 'invalid' : '';
+
+  const emailControlDivClassName = !isValidEnteredEmail && isTouchedEnteredEmail ? 'invalid' : '';
 
   const nameValidationErrorMessage = !isValidEnteredName &&
     isTouchedEnteredName && (
       <SimpleInputErrorTextP>Name must not be empty.</SimpleInputErrorTextP>
     );
 
+  const emailValidationErrorMessage = !isValidEnteredEmail &&
+    isTouchedEnteredEmail && (
+      <SimpleInputErrorTextP>Please enter valid email.</SimpleInputErrorTextP>
+    );
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <SimpleInputControlDiv className={controlDivClassName}>
+      <SimpleInputControlDiv className={nameControlDivClassName}>
         <SimpleInputLabel htmlFor="name">Your name</SimpleInputLabel>
         <SimpleInputInput
           ref={nameInputRef}
@@ -79,6 +102,17 @@ function SimpleInput() {
           onBlur={nameInputBlurHandler}
         />
         {nameValidationErrorMessage}
+      </SimpleInputControlDiv>
+      <SimpleInputControlDiv className={emailControlDivClassName}>
+        <SimpleInputLabel htmlFor="email">E-mail Address</SimpleInputLabel>
+        <SimpleInputInput
+          type="email"
+          id="email"
+          value={enteredEmail}
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+        />
+        {emailValidationErrorMessage}
       </SimpleInputControlDiv>
       <SimpleInputActionsDiv>
         <SimpleInputButton disabled={!isValidForm}>Submit</SimpleInputButton>
