f14f1cb1117f04e11b8687a54f3c0ce006fc97c5
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-07 23:45:33 +0900

    apply use-basicform-input to BasicForm

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -6,22 +6,62 @@ import {
   BasicFormInput,
   BasicFormLabel,
 } from '@/components/BasicFormStyle';
+import { useBasicFormInput } from '@/hooks/use-basicform-input';
 
 function BasicForm() {
+  const {
+    enteredValue: enteredFirstName,
+    isTouched: isTouchedFirstName,
+    isValidValue: isValidFirstName,
+    valueInputChangeHandler: firstNameInputChangeHandler,
+    valueInputBlurHandler: firstNameInputBlurHander,
+  } = useBasicFormInput({ type: 'name' });
+  const {
+    enteredValue: enteredLastName,
+    isTouched: isTouchedLastName,
+    isValidValue: isValidLastName,
+    valueInputChangeHandler: lastNameInputChangeHandler,
+    valueInputBlurHandler: lastNameInputBlurHander,
+  } = useBasicFormInput({ type: 'name' });
+  const {
+    enteredValue: enteredEmail,
+    isTouched: isTouchedEmail,
+    isValidValue: isValidEmail,
+    valueInputChangeHandler: emailInputChangeHandler,
+    valueInputBlurHandler: emailInputBlurHandler,
+  } = useBasicFormInput({ type: 'email' });
   return (
     <form>
       <BasicFormDiv>
         <BasicFormControlDiv>
           <BasicFormLabel htmlFor="first-name">First Name</BasicFormLabel>
-          <BasicFormInput type="text" id="first-name" />
+          <BasicFormInput
+            type="text"
+            id="first-name"
+            value={enteredFirstName}
+            onChange={firstNameInputChangeHandler}
+            onBlur={firstNameInputBlurHander}
+          />
         </BasicFormControlDiv>
         <BasicFormControlDiv>
           <BasicFormLabel htmlFor="last-name">Last Name</BasicFormLabel>
-          <BasicFormInput type="text" id="last-name" />
+          <BasicFormInput
+            type="text"
+            id="last-name"
+            value={enteredLastName}
+            onChange={lastNameInputChangeHandler}
+            onBlur={lastNameInputBlurHander}
+          />
         </BasicFormControlDiv>
         <BasicFormControlDiv>
           <BasicFormLabel htmlFor="email">E-mail Addresss</BasicFormLabel>
-          <BasicFormInput type="email" id="email" />
+          <BasicFormInput
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailInputChangeHandler}
+            onBlur={emailInputBlurHandler}
+          />
         </BasicFormControlDiv>
       </BasicFormDiv>
       <BasicFormActionsDiv>
